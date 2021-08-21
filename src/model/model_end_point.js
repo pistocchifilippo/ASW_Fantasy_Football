70d04258fea9c991d59e2555cf6aeb540be9ca77
pistocchifilippo <filippo.pistocchi4@studio.unibo.it>
2021-08-21 10:56:24 +0200

    match -> pattern

--- src/model/model_end_point.js
+++ src/model/model_end_point.js
@@ -4,13 +4,13 @@ const routes = require("./routes.json")
 const mongo_settings = require("./mongo_settings.json")
 const MongoClient = require('mongodb').MongoClient
 
-const get_mongo_item_by_id = async (match,res,collection) => {
+const get_mongo_item_by_id = async (pattern,res,collection) => {
     MongoClient.connect(mongo_settings.mongo_address + mongo_settings.mongo_db, (err, client) => {
         if(err) throw err
     
         // Connect to the db
         var db = client.db(mongo_settings.mongo_db);
-        db.collection(collection).find(match).toArray((err, items) => {
+        db.collection(collection).find(pattern).toArray((err, items) => {
             if(err) throw err;    
             res.json(items)
             client.close()           
@@ -19,12 +19,12 @@ const get_mongo_item_by_id = async (match,res,collection) => {
 }
 
 app.get(routes.teams_route, (req,res) => {
-    const match = {"_id":parseInt(req.params.id)}
-    get_mongo_item_by_id(match,res,mongo_settings.teams_collection)
+    const pattern = {"_id":parseInt(req.params.id)}
+    get_mongo_item_by_id(pattern,res,mongo_settings.teams_collection)
 })
 app.get(routes.players_route, (req,res) => {
-    const match = {"_id":parseInt(req.params.id)}
-    get_mongo_item_by_id(match,res,mongo_settings.players_collection)
+    const pattern = {"_id":parseInt(req.params.id)}
+    get_mongo_item_by_id(pattern,res,mongo_settings.players_collection)
 })
 
 app.use((req,res,next) => {

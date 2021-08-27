3ef2fa408f550a72433242a10c86956ca0721bf2
pistocchifilippo <filippo.pistocchi4@studio.unibo.it>
2021-08-27 18:40:20 +0200

    moved routes in controller

--- src/controller/routes.js
+++ src/controller/routes.js
@@ -2,19 +2,19 @@ const express = require("express")
 const app = express()
 const routes = require("./routes.json")
 
-const mongo_settings = require("./mongo/mongo_settings.json")
-const mongo_query = require("./mongo/mongo_functions.js")
+const collections = require("./collections.json")
+const mongo_query = require("../model/mongo/mongo_functions.js")
 
 app.use(express.json());
 
 app.get(routes.teams_route, async (req,res) => {
     const query = req.body
-    const x = await mongo_query.find_one(query,mongo_settings.teams_collection)
+    const x = await mongo_query.find_one(query,collections.teams_collection)
     res.json(x)
 })
 app.get(routes.players_route, async (req,res) => {
     const query = req.body
-    const x = await mongo_query.find_one(query,mongo_settings.players_collection)
+    const x = await mongo_query.find_one(query,collections.players_collection)
     res.json(x)
 })
 app.post(routes.leagues, async (req,res) => {
@@ -23,7 +23,7 @@ app.post(routes.leagues, async (req,res) => {
     if (league.hasOwnProperty("name") && league.hasOwnProperty("admin_id") && league.hasOwnProperty("player_limit")) 
     {
         league.members=[league.admin_id]
-        await mongo_query.insert_one(league,mongo_settings.leagues_collection)
+        await mongo_query.insert_one(league,collections.leagues_collection)
         res.sendStatus(200)
     }
     else 
@@ -33,7 +33,7 @@ app.post(routes.leagues, async (req,res) => {
 })
 app.get(routes.leagues, async (req,res) => {
     const query = req.body
-    const x = await mongo_query.find_one(query,mongo_settings.leagues_collection)
+    const x = await mongo_query.find_one(query,collections.leagues_collection)
     res.json(x)
 })
 
--- src/model/prova.js
+++
@@ -1,4 +0,0 @@
-const mongo = require("./mongo/mongo_functions.js")
-
-// mongo.find_one({name:"Cristiano Ronaldo"},"Players").then(console.log)
-mongo.update({name:"The best"},{"player_limit":1},"Leagues")
